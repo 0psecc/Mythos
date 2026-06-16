82e34d8d69bdb3b43d3c341869bd86b54c72c1ef
krunkify <henrydahles7@gmail.com>
2026-06-16 11:46:29 +0200

    Update download

--- download.php
+++ download.php
@@ -1,38 +1,4 @@
 <?php
-<<<<<<< HEAD:download.php
-$servername = "localhost"; 
-$dbname = "mythos";
-$username = "root";
-$password = "";
-
-try {
-    $dsn = "mysql:host=$servername;dbname=$dbname;port=3306;charset=utf8mb4";
-
-    $conn = new PDO($dsn, $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 10, // prevents long hanging attempts
-        PDO::ATTR_PERSISTENT => false,
-    ]);
-
-    echo "Connected successfully";
-
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
-
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mythos</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-</body>
-</html>
-=======
 $uploadmap = "uploads/";
 
 if (isset($_GET["file"])) {
@@ -51,4 +17,4 @@ if (isset($_GET["file"])) {
 } else {
     echo "Geen bestand opgegeven.";
 }
->>>>>>> 0e61aaafcb81593725a1067795d1473f446b5d08:.vscode/download.php
+?>
