e5cf7962d5f9ac11a5c216a79b74ede4fa0cfba1
SwimmingTrunk <nikartiel@gmail.com>
2026-06-16 10:30:11 +0200

    Database connection

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 $servername = "localhost";
-$username = "username";
+$username = "root";
 $password = "";
 $dbname = "mythos";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
 ?>
 
 
